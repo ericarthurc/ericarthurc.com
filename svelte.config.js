02aa80394a4137e48b3ae6b743c2a4b0bf9f4b6a
Eric Christensen <hebrew.8.12@gmail.com>
2024-10-27 13:46:23 -0700

    13:46 10-27

--- svelte.config.js
+++ svelte.config.js
@@ -4,16 +4,16 @@ import { mdsvex } from 'mdsvex';
 import rehypeSlug from 'rehype-slug';
 import { bundledLanguages, createHighlighter } from 'shiki';
 
+const shiki = await createHighlighter({
+	themes: ['andromeeda'],
+	langs: [...Object.keys(bundledLanguages)]
+});
+
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
 	highlight: {
 		highlighter: async (code, lang, meta) => {
-			const shiki = await createHighlighter({
-				themes: ['andromeeda'],
-				langs: [...Object.keys(bundledLanguages)]
-			});
-
 			return shiki.codeToHtml(code, {
 				lang,
 				theme: 'andromeeda',
