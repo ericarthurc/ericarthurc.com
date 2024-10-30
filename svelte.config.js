162130d80145132a7062ad3a992687ba244459c6
Eric Christensen <hebrew.8.12@gmail.com>
2024-10-30 14:30:06 -0700

    14:30 10-30

--- svelte.config.js
+++ svelte.config.js
@@ -92,7 +92,7 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
-		// inlineStyleThreshold: 10240,
+		inlineStyleThreshold: 10240,
 		adapter: adapter(),
 		alias: {
 			'#': './src/components',
