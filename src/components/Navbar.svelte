6665f0c8c10228517d3dde96fca6e845b588e0f4
Eric Christensen <hebrew.8.12@gmail.com>
2024-11-07 11:47:46 -0800

    11:47 11-07

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -7,25 +7,27 @@
 		<svg
 			width="100%"
 			height="100%"
-			viewBox="0 0 656 670"
+			viewBox="0 0 767 767"
 			version="1.1"
 			xmlns="http://www.w3.org/2000/svg"
 			xmlns:xlink="http://www.w3.org/1999/xlink"
 			xml:space="preserve"
-			style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"
+			style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"
 		>
-			<g transform="matrix(1,0,0,1,-172.267,-165.25)">
+			<g transform="matrix(1,0,0,1,-116.5,-116.5)">
+				<g transform="matrix(1,0,0,1,3.625,46.625)">
+					<circle id="bg" cx="496.375" cy="453.375" r="381.375" style="stroke-width:4.25px;" />
+				</g>
 				<g transform="matrix(1,0,0,1,-7.10543e-14,6.25)">
 					<g transform="matrix(0.984925,0,0,0.984925,5.07538,11.2613)">
-						<circle id="top" cx="502.5" cy="249.5" r="99.5" style="fill:currentcolor" />
+						<circle id="cir-top" cx="502.5" cy="249.5" r="99.5" />
 					</g>
 					<g transform="matrix(0.984925,0,0,0.984925,5.07538,254.261)">
-						<circle id="bottom" cx="502.5" cy="249.5" r="99.5" style="fill:currentcolor" />
+						<circle id="cir-bottom" cx="502.5" cy="249.5" r="99.5" />
 					</g>
 					<path
-						id="outer"
+						id="base"
 						d="M291.345,462L172.267,462L172.267,500.767C172.267,681.648 319.119,828.5 500,828.5C680.881,828.5 827.733,681.648 827.733,500.767L827.733,462L708.655,462L708.655,355L643.655,355L643.655,499.345C643.655,578.63 579.285,643 500,643C420.715,643 356.345,578.63 356.345,499.345L356.345,355L291.345,355L291.345,462ZM705.075,536C686.922,632.723 601.951,706 500,706C398.049,706 313.078,632.723 294.925,536L247.99,536C264.511,660.394 371.121,756.5 500,756.5C628.879,756.5 735.489,660.394 752.01,536L705.075,536Z"
-						style="fill:currentcolor;"
 					/>
 				</g>
 			</g>
